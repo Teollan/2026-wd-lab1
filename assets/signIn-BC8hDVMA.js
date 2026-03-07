98787ed154c1e017bcc9330a319c707c94d14e56
Serhii Kozlov <serhii.kozlov.work@gmail.com>
2026-03-07 20:44:20 +0200

    Updates

---
+++ assets/signIn-BC8hDVMA.js
@@ -0,0 +1,1 @@
+import"./menu-Brqr5gl4.js";document.getElementById("sign-in-form").addEventListener("submit",()=>{localStorage.setItem("auth","true")});
---
+++ assets/signUp-CHDBlZlf.js
@@ -0,0 +1,1 @@
+import"./menu-Brqr5gl4.js";document.getElementById("sign-up-form").addEventListener("submit",()=>{localStorage.setItem("auth","true")});
